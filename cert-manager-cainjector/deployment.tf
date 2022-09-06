e227f26250313bd13430156a18a2e14f10ebb658
Nathan Colgate <nathancolgate@gmail.com>
2022-09-06 11:48:57 -0500

    adding name and namespace back

--- cert-manager-cainjector/deployment.tf
+++ cert-manager-cainjector/deployment.tf
@@ -1,5 +1,7 @@
 resource "kubernetes_deployment" "deployment" {
   metadata {
+    name      = var.name
+    namespace = var.namespace
     labels = merge({
       "app.kubernetes.io/name" = var.name
     }, local.labels)
